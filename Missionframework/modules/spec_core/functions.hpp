8375d9ec765c814b0002dd9cb23cc0d7ef9b732c
Reidond <reidond@gmail.com>
2021-02-22 22:55:31 +0200

    feat(spec_core): add starting vehicle and respawn

---
+++ Missionframework/modules/spec_core/functions.hpp
@@ -0,0 +1,21 @@
+class core {
+    file = "modules\spec_core\fnc";
+
+    class core_spawnStartVeh {};
+
+    // Module post initiali~zation
+    class core_postInit {
+        postInit = 1;
+    };
+
+    // Module pre initialization
+    class core_preInit {
+        preInit = 1;
+    };
+
+    class core_handleVehicleSpawn {};
+
+    class core_handleVehicleRespawn {};
+
+    class core_settings {};
+};
